e523a043db9312d411b8fad1f62e409b58b081ce
Roberto Lopez-trigo <bobby@Robertos-MacBook-Pro.local>
2022-05-19 13:38:05 -0400

    IT works

---
+++ bot.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import os
+import discord
+from discord.ext import commands,tasks
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+bot = commands.Bot("!")
+
+
+
+@tasks.loop(hours=24)
+async def called_once_a_day():
+    message_channel = bot.get_channel(int(CHANNEL_ID))
+    print(f"Got channel {message_channel}")
+    if datetime.now().weekday() <= 4:
+        await message_channel.send("@everyone Please Complete your Daily Stand-up by 8pm. BEEP BOOP Here is the Link: https://drive.google.com/drive/folders/1yWNESub8Ya61OzLj0Wzkrg1MsVWaoqSy BEEP BOOP ")
+
+@called_once_a_day.before_loop
+async def before():
+    await bot.wait_until_ready()
+    print("Finished waiting")
+
+called_once_a_day.start()
+bot.run(TOKEN)
+
